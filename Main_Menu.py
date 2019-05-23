61f029f5bf92fa5b749b103af1e12e16464cebf9
한일석 <x2ever@naver.com>
2019-05-24 00:26:00 +0900

    Update Main_Menu.py

--- Main_Menu.py
+++ Main_Menu.py
@@ -4,13 +4,13 @@ from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((1024, 768))
-while 1:
+while True:
     screen.fill((0,0,0))
     for event in pygame.event.get():
-                if not hasattr(event, 'key'): continue
-                if event.key == K_SPACE: 
-                    main.level1()
-                elif event.key == K_ESCAPE: sys.exit(0)  
+        if not hasattr(event, 'key'): continue
+        if event.key == K_SPACE: 
+            main.level1()
+        elif event.key == K_ESCAPE: sys.exit(0)  
     img = pygame.image.load("images/main_menu_image.png")
-    screen.blit(img,(0,0))
+    screen.blit(img, (0, 0))
     pygame.display.flip()
