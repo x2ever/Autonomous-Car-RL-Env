9e1699a0847427012275ede1fe69edb0f10150c7
한일석 <x2ever@naver.com>
2019-05-24 17:44:41 +0900

    Create Trophy.py

---
+++ Trophy.py
@@ -0,0 +1,11 @@
+import pygame
+
+class TrophySprite(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/trophy.png')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
