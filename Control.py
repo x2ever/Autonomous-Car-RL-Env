eb9115acd4860b167dde6c83aa2c58e41b2f8431
한일석 <x2ever@naver.com>
2019-05-24 17:43:50 +0900

    Create Control.py

---
+++ Control.py
@@ -0,0 +1,15 @@
+class Control:
+    def __init__(self):
+        self.instructions = []
+    
+    def up(self):
+        pass
+    
+    def down(self):
+        pass
+
+    def right(self):
+        pass
+    
+    def left(self):
+        pass
