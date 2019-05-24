d0693a42ba0dd3d80488a08c8947dce54a11f79c
한일석 <x2ever@naver.com>
2019-05-24 22:52:50 +0900

    Update Brain.py

--- Brain.py
+++ Brain.py
@@ -1,18 +1,33 @@
+import time
+import pygame
+
+from pygame.locals import *
+
 class Brain:
-    def __init__(self, lidar, control):
-        self.lidar = lidar
-        self.control = control
+    def __init__(self, database):
+        self.database = database
 
     def run(self):
         while True:
-            # Implement Your Algorithm
+            # Implement Your Algorithm HERE!!
             ''' 
-            1. How To Get Lidar Data
-                data = self.lidar.data
-            2. How can i move car
-                self.control.up()
-                self.control.down()
-                self.control.right()
-                self.control.left()
+            1. How can i get a lidar data?
+                data = self.database.lidar.data
+
+            2. How can i move a car?
+                self.database.control.up()
+                self.database.control.down()
+                self.database.control.right()
+                self.database.control.left()
+
+            3. How can i get a car status data?
+                self.database.car.direction
+                self.database.car.speed
             '''
-            pass
+            print(self.database.car.speed, self.database.car.direction)
+            self.database.control.up()
+            self.database.control.right()
+
+            time.sleep(0.01)
+            if self.database.stop:
+                break
