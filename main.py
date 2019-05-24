c8e7a2774bb821208a6d16e8fcedf7a1e87eaf1e
한일석 <x2ever@naver.com>
2019-05-24 22:53:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
+import pygame
 import threading
+import argparse
 
 from Game import Game 
 from Wall import WallSprite
@@ -8,9 +10,9 @@ from Trophy import TrophySprite
 from LiDAR import LiDAR
 from Brain import Brain
 from Control import Control
+from Database import Database
 
-
-def main():
+def main(auto):
     walls = [
         WallSprite((512, 2.5), 1024, 5),
         WallSprite((512, 765.5), 1024, 5),
@@ -23,15 +25,21 @@ def main():
     car = CarSprite('images/car.png', (50, 700))
     lidar = LiDAR()
     control = Control()
-    brain = Brain(lidar, control) # Get LiDAR data, Set Control data
-    game = Game(walls, trophies, car, lidar, control) # Get Control data Set LiDAR data
-    game.run()
-    while True:
-        time.sleep(0.1)
-        if game.stop:
-            break
-    time.sleep(2)
+    database = Database(lidar, control, car)
+    brain = Brain(database) # Get LiDAR data, Set Control data
+    game = Game(walls, trophies, car, database) # Get Control data Set LiDAR data
+    if auto:
+        brain_thread = threading.Thread(target=brain.run,)
+        brain_thread.start()
+
+    game.run(auto=auto)
+    pygame.quit()
+
     return 0
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--auto", help="Do not use your keyboard command, but use pre-defined brain's command.",
+                    action="store_true", default=False)
+    args = parser.parse_args()
+    main(args.auto)
