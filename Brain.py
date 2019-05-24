5ad1961d80a78713071ed74e8b3bb8dc403ffa96
한일석 <x2ever@naver.com>
2019-05-24 17:43:44 +0900

    Create Brain.py

---
+++ Brain.py
@@ -0,0 +1,18 @@
+class Brain:
+    def __init__(self, lidar, control):
+        self.lidar = lidar
+        self.control = control
+
+    def run(self):
+        while True:
+            # Implement Your Algorithm
+            ''' 
+            1. How To Get Lidar Data
+                data = self.lidar.data
+            2. How can i move car
+                self.control.up()
+                self.control.down()
+                self.control.right()
+                self.control.left()
+            '''
+            pass
