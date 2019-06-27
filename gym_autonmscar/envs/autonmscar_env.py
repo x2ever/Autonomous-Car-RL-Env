5e40405ffaba6b81533fcedc9fa0ab438ea5c6dd
Jueun <elpion19@gmail.com>
2019-06-28 01:33:51 +0900

    action and obs space

--- gym_autonmscar/envs/autonmscar_env.py
+++ gym_autonmscar/envs/autonmscar_env.py
@@ -2,17 +2,26 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class AutonomousCarEnv(gym.Env):
-    metadate = {'render.modes': ['human']}
+from gym_autonmscar.envs.autonomous_car_simulator.Brain import Brain
+
+
+class AutonomousCarEnv(gym.Env, Brain):
+    metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        pass
-    
+        self.action_space = spaces.Discrete(4)  # up, down, left, right
+
+        L = 100  # in LiDAR.py
+        # (360 for LiDAR data) + (2 for the status of car)
+        self.observation_size = self.database.lidar.data.shape[0] + 2
+        self.observation_space = spaces.Box(
+            low=0, high=L, shape=(self.observation_size, 0))
+
     def step(self, action):
         pass
-    
+
     def reset(self):
         pass
-    
+
     def render(self, mode='human', close=False):
         pass
