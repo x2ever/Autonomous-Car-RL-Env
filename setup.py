f83969e40b007bbfc5844b04a300d858caae3c24
Jueun-Park <elpion19@gmail.com>
2019-07-01 12:46:10 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_autonmscar',
       version='0.0.1',
-      install_requires=['gym', 'pygame']
+      install_requires=['gym', 'pygame', 'tensorflow', 'stable-baselines']
       )
